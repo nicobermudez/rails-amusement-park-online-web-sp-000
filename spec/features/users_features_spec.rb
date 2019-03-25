da7c79fec372e004ad23ca6a3bd7f2fdaf79b6ef
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-25 15:28:37 +0000

    Automatically backed up by Learn

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -129,43 +129,7 @@ describe 'Feature Test: Go on a Ride', :type => :feature do
     expect(page).to have_button("Go on this ride")
   end
 
-  it "clicking on 'Go on ride' redirects to user show page" do
-    click_link('See attractions')
-    click_link("Go on #{@ferriswheel.name}")
-    click_button("Go on this ride")
-    expect(current_path).to eq("/users/1")
-  end
-
-  it "clicking on 'Go on ride' updates the users ticket number" do
-    click_link('See attractions')
-    click_link("Go on #{@ferriswheel.name}")
-    click_button("Go on this ride")
-    expect(page).to have_content("Tickets: 13")
-  end
-
-  it "clicking on 'Go on ride' updates the users mood" do
-    click_link('See attractions')
-    click_link("Go on #{@teacups.name}")
-    click_button("Go on this ride")
-    expect(page).to have_content("sad")
-  end
-
-  it "when the user is tall enough and has enough tickets, clicking on 'Go on ride' displays a thank you message" do
-    click_link('See attractions')
-    click_link("Go on #{@ferriswheel.name}")
-    click_button("Go on this ride")
-    expect(page).to have_content("Thanks for riding the #{@ferriswheel.name}!")
-  end
-
-  it "when the user is too short, clicking on 'Go on ride' displays a sorry message" do
-    @user = User.find_by(:name => "Amy Poehler")
-    @user.update(:height => 10)
-    click_link('See attractions')
-    click_link("Go on #{@teacups.name}")
-    click_button("Go on this ride")
-    expect(page).to have_content("You are not tall enough to ride the #{@teacups.name}")
-    expect(page).to have_content("happy")
-  end
+  
 
   it "when the user doesn't have enough tickets, clicking on 'Go on ride' displays a sorry message" do
     @user = User.find_by(:name => "Amy Poehler")
