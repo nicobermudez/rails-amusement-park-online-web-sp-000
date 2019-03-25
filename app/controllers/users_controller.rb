2d8ed7368cfedcd341c5f2a06aa3f86bf36785fd
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-25 14:19:51 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
+    @message = params[:message] if params[:message]
+    @message ||= false
   end
 
   def new
@@ -9,9 +11,28 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to user_path(@user), notice: "Welcome to the theme park!" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
   end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   private
