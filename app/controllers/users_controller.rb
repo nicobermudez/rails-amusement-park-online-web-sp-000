45a225f8b97f3a09b3a15d76a79c64250d1f5ecd
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-25 14:14:42 +0000

    Automatically backed up by Learn

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params(:user).permit(
+      :name,
+      :password,
+      :height,
+      :tickets,
+      :happinesss,
+      :nausea,
+      :admin
+      )
+    end
+end
